7da2e4a6522f4909cda1a8ed371fd0b2240b8f54
Tatmiki <tatmiki@outlook.com>
2026-03-13 12:47:36 -0300

    refactor: modifica url do next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  assetPrefix: "/semec/declaracao-itbi-rural",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
